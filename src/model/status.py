5ce1e82c826bb5f9200d5da0faa9ba2d1669ed42
Jwuan <18818747256@163.com>
2026-04-20 22:48:46 +0800

    1

--- src/model/status.py
+++ src/model/status.py
@@ -24,5 +24,32 @@ class GPIN :
 
         elif self.mode == 0:
             GPIO.setup(self.pin, GPIO.IN)
+    #输出控制
+    def set_value(self, value):
+        if self.mode == 1:
+            GPIO.output(self.pin, GPIO.HIGH if value == 1 else GPIO.LOW)
+        else:
+            print(f"错误: 引脚 {self.pin} 是输入模式，无法设置值")
+    #闪烁
+    def flash(self):
+            times += 1
+            self.times = self.times % 10#让计数在 0-9 之间循环
 
-    def 
+            #每当times==0时电平改变一次
+            if times == 0:
+                if self.status == GPIO.LOW:
+                      self.status = GPIO.HIGH
+                
+                elif self.status == GPIO.HIGH:
+                     self.status = GPIO.LOW
+
+            GPIO.output(self.pin, self.status)
+
+    def read_status(self):
+         # 检查模式是否为1，如果是则返回错误
+        if self.mode == 1:
+            print("错误的引脚类型!!!!!!!!!!!!!!!!!!!!!!!")
+            return None
+        
+        # 检测按钮按下（低电平）
+        return int(GPIO.input(self.pin) == GPIO.LOW)
